2445c2859ebc947f2bca2f6cc16303eaf533dd8b
kubwimanav <kubwimanavincent2020@gmail.com>
2025-02-06 13:04:50 +0200

    diplay most used character

--- challenge.js
+++ challenge.js
@@ -29,4 +29,32 @@ function paradrom(str) {
 }
 console.log(paradrom('madam'));
 
+// diplay character which is most used "abcccccccccddddd"
+
+function maxchar(str) {
+    const charmap = {};
+    let max = 0;
+    let machar = '';
+
+    for (let char of str) {
+        if (charmap[char]) {
+            charmap[char] = charmap[char] + 1;
+        }
+        else {
+            charmap[char] = 1;
+        }
+        
+    }
+
+    for (let char in charmap) {
+        if (charmap[char] > max) {
+            max = charmap[char];
+            machar = char;
+        }
+    }
+
+    return max;
+}
+console.log(maxchar('aaaassssssssssssssssbbbbbbbbbbbbbbbhhhhhhhhhhhhhhhhh'));
+
 
