99985e81ec7067249736b0b993f3dc57cfaed00d
kubwimanav <kubwimanavincent2020@gmail.com>
2025-02-06 11:49:30 +0200

    reverse challenge

---
+++ challenge.js
@@ -0,0 +1,19 @@
+
+// reverse challenge
+
+function reversed(str) {
+    let reverse = '';
+    for (let char of str){
+        reverse = char + reverse;
+    }
+
+    // for (let i = 0; i < str.length; i++){
+    //     reverse = str[i] + reverse;
+    // } same as above will get some result
+    return reverse;
+}
+
+console.log(reversed('my friend'));
+
+
+
