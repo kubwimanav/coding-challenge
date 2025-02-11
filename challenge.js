e108ec78fcc59d4370b9408a366013587a100613
kubwimanav <kubwimanavincent2020@gmail.com>
2025-02-11 17:35:49 +0200

    third commit

--- challenge.js
+++ challenge.js
@@ -68,3 +68,22 @@ function chunk(arry, size) {
 
 console.log(chunk([1,2,3,4,5,6,7,8,9], 3));
 
+// array method
+// 1.length: to the length of the array
+// 2.toString: to convert the array to string
+// 3.at: to get the element at the given index
+// 4.pop: to remove the last element from the array
+// 5.push: to add the element to the end of the array
+// 6.shift: to remove the first element from the array
+// 7.unshift: to add the element to the beginning of the array
+// 8.concat: to merge two or more arrays
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const fruits1 = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.concat(fruits1));
+
+function division (num1, num2) {
+    return num1 / num2;
+}
+
+console.log(division(10, 2));
