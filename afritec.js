38883f4e9d81bf392b3dae95f2152641710478cc
kubwimanav <kubwimanavincent2020@gmail.com>
2025-02-11 19:57:02 +0200

    third commit

---
+++ afritec.js
@@ -0,0 +1,27 @@
+let character = "hello";
+console.log(character);
+character = 'world';
+console.log(character);
+
+secondCharacter = character;
+console.log(secondCharacter);
+
+let profession ;
+let age;
+profession = 'teacher';
+age = 25;
+console.log(profession);
+console.log(age);
+
+let chsracter = 'hello';
+let number = 25;
+console.log(number+1);
+
+
+let array = [1, 2, 3];
+array[1] = 25;
+console.log(array);
+
+
+let cities = ['lagos', 'abuja', 'kano'];
+console.log(cities);
